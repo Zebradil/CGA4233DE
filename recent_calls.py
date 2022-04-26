c8bdf43184ac996a05670b07a94295bbe37857cd
German Lashevich <zebradil@komputer.localdomain>
2022-04-26 14:23:25 +0200

    Reformat with black and isort

--- recent_calls.py
+++ recent_calls.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-from CGA4233DE import CGA4233DE
+
 import argparse
 
+from CGA4233DE import CGA4233DE
+
 parser = argparse.ArgumentParser()
-parser.add_argument('-a', '--address', type=str, default='http://192.168.0.1')
-parser.add_argument('-u', '--user', type=str, default='admin')
-parser.add_argument('password', type=str)
+parser.add_argument("-a", "--address", type=str, default="http://192.168.0.1")
+parser.add_argument("-u", "--user", type=str, default="admin")
+parser.add_argument("password", type=str)
 args = parser.parse_args()
 
 router = CGA4233DE(args.address, args.user, args.password)
